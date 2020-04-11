62d68eea1457ea68e0e1dba5dd74924b62a5f421
R0quef0rt <ryanjbrooks11@gmail.com>
2020-04-11 16:56:45 -0500

    Chapter 3

---
+++ collatz.py
@@ -0,0 +1,17 @@
+print('Input a number')
+n = int(input())
+
+def collatz(number):
+    if (number % 2) == 0:
+        print(number // 2)
+        return (number // 2)
+    elif (number % 2) == 1:
+        print(3 * number + 1)
+        return (3 * number + 1)
+
+def repeatCollatz(number):
+    while number != 1:
+        number = collatz(number)
+        number // 2
+
+repeatCollatz(n)
