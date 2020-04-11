5dfbba5925932015388b8dc939fc89a31bf04f09
R0quef0rt <ryanjbrooks11@gmail.com>
2020-04-11 00:01:56 -0500

    Chapter 2

--- hello.py
+++ hello.py
@@ -1,4 +1,18 @@
 # This program says hello and asks for my name.
 
-number = 25.465424
-print( str(round(number,3)) + ' (Rounded to the nearest 3 digits)')
+print('What is your name?')
+myName = input()
+print('What is your age?')
+myAge = int(input())
+if myName == 'Ryan' and myAge == 31:
+    print('Success!')
+else:
+    print('Failure!')
+
+i = 0
+while myName == 'Ryan' and i < 5:
+    print('Hooray!')
+    i = i + 1
+
+for n in range(len(myName)):
+    print(str(n) + ' Boops!')
