47e8ed4cf5cbfbe3d9496d91ef995812f958dc3c
R0quef0rt <ryanjbrooks11@gmail.com>
2020-04-22 20:23:45 -0500

    Chapter 11

---
+++ coinToss.py
@@ -0,0 +1,23 @@
+import random
+guess = ''
+while guess not in ('heads', 'tails'):
+    print('Guess the coin toss! Enter heads or tails:')
+    guess = input()
+if guess == 'tails':
+    guess = 0
+else:
+    guess = 1
+toss = random.randint(0, 1) # 0 is tails, 1 is heads
+if toss == guess:
+    print('You got it!')
+else:
+    print('Nope! Guess again!')
+    guess = input()
+    if guess == 'tails':
+        guess = 0
+    else:
+        guess = 1
+    if toss == guess:
+        print('You got it!')
+    else:
+        print('Nope. You are really bad at this game.')
